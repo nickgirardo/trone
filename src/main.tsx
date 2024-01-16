ac17a82a8340917798055b490bc344e1c799ebd9
Nick Girardo <nickgirardo@gmail.com>
2024-01-16 18:03:17 -0500

    Disable strictmode because of dnd bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store";
@@ -9,9 +8,7 @@ import "./index.css";
 Modal.setAppElement("#root");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
