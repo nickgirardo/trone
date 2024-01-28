f0df94a4b06a6a28cacbbcb4b0b6a2b578e904c9
Nick Girardo <nickgirardo@gmail.com>
2024-01-27 22:18:20 -0500

    Style improvements for new card

--- src/components/CardList/NewCard.tsx
+++ src/components/CardList/NewCard.tsx
@@ -1,6 +1,8 @@
-import { ChangeEvent, useState } from "react";
+import { useState } from "react";
 import { useAppDispatch } from "../../store";
 import { createCard } from "../../reducers/Cards";
+
+import "./new-card.scss";
 
 export const NewCard = ({ list }: { list: string }) => {
   const dispatch = useAppDispatch();
@@ -15,23 +17,23 @@ export const NewCard = ({ list }: { list: string }) => {
   return (
     <div className="new-card">
       <form onSubmit={(e) => e.preventDefault()}>
-        <label>
-          Create a new card
+        <label htmlFor={`new-card-${list}`}>Create a new card</label>
+        <div className="inputs">
           <input
             value={name}
-            onChange={(ev: ChangeEvent<HTMLInputElement>) =>
-              setName(ev.target.value)
-            }
+            type="text"
+            id={`new-card-${list}`}
+            onChange={(ev) => setName(ev.target.value)}
             placeholder="Card text"
             required
           />
-        </label>
-        <input
-          type="submit"
-          onClick={handleCreateCard}
-          value="Ok"
-          disabled={name.length === 0}
-        />
+          <input
+            type="submit"
+            onClick={handleCreateCard}
+            value="Ok"
+            disabled={name.length === 0}
+          />
+        </div>
       </form>
     </div>
   );
